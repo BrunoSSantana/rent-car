5915d8d216f603c73a4fb6ce3571a3cf00a741ed
BrunoSSantana <brunoosouza15@gmail.com>
2021-09-21 19:34:00 -0300

    Add route auth client

--- backend/src/routes/clients.routes.ts
+++ backend/src/routes/clients.routes.ts
@@ -1,10 +1,13 @@
+import { AuthClientController } from '@modules/clients/controllers/AuthClientContoller'
 import { CreateClientController } from '@modules/clients/controllers/CreateClientController'
 import { Router } from 'express'
 
 const clientsRoutes = Router()
 
 const createClientController = new CreateClientController()
+const authClientController = new AuthClientController()
 
 clientsRoutes.post('/new', createClientController.handle)
+clientsRoutes.post('/auth', authClientController.handle)
 
 export { clientsRoutes }
