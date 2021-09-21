6a1282b4669865fce7eed1235f8170465580da15
BrunoSSantana <brunoosouza15@gmail.com>
2021-09-21 13:06:19 -0300

    create connection typeorm/database

--- backend/src/data/index.ts
+++ backend/src/data/index.ts
@@ -1,3 +1,3 @@
-import { createConnection } from 'typeorm';
+import { createConnection } from 'typeorm'
 
-createConnection().then(() => console.log('Conect Database!'));
+createConnection().then(() => console.log('Conect Database!'))
