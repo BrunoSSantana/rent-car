40e047054911f755ee45bd4e409eb9b55af0ec87
Jonata Ramalho <jonataramalho.s@gmail.com>
2021-09-22 18:42:38 -0300

    feat: configura styled-reset e styled-components

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,13 +1,19 @@
-import { Switch, Route } from 'react-router-dom'
-import Home from './pages/Home';
+import { Switch, Route } from "react-router-dom";
+import Home from "./pages/Home/";
+import { ThemeProvider } from "styled-components";
+import { Reset } from "styled-reset";
+
+import theme from "./theme";
 
 function App() {
-
   return (
     <>
-      <Switch>
-        <Route path='/' exact component={Home} />
-      </Switch>
+      <ThemeProvider theme={theme}>
+        <Reset />
+        <Switch>
+          <Route path="/" exact component={Home} />
+        </Switch>
+      </ThemeProvider>
     </>
   );
 }
