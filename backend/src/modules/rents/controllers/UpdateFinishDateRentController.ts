d74da7b5d0343a3131690d5981efd32f47a4345d
BrunoSSantana <brunoosouza15@gmail.com>
2021-09-23 22:16:52 -0300

    Add routes rents update and finally

--- backend/src/modules/rents/controllers/UpdateFinishDateRentController.ts
+++ backend/src/modules/rents/controllers/UpdateFinishDateRentController.ts
@@ -1,22 +1,19 @@
-import { CarsRepositories } from '@modules/cars/repositories/CarsRepositories'
 import { Request, Response } from 'express'
 import { getCustomRepository } from 'typeorm'
 
 import { RentsRepositories } from '../repositories/RentsRepositories'
 
-class FinallyRentController {
+class UpdateFinishDateRentController {
   async handle(request: Request, response: Response): Promise<Response> {
     const rentsRepository = getCustomRepository(RentsRepositories)
-    const carsRepository = getCustomRepository(CarsRepositories)
 
-    const { id } = request.body
+    const { id, finish_date } = request.body
 
+    await rentsRepository.update(id, { finish_date })
     const rent = await rentsRepository.findOne(id)
 
-    carsRepository.updateAvailable(rent.car_id, true)
-
-    return response.json()
+    return response.json(rent)
   }
 }
 
-export { FinallyRentController }
+export { UpdateFinishDateRentController }
