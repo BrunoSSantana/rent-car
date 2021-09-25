b4ad6fc9cf47e3ae35f09e1de635da06f607cadc
BrunoSSantana <brunoosouza15@gmail.com>
2021-09-25 12:51:14 -0300

    Move download router

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -3,7 +3,6 @@ import 'express-async-errors'
 
 import cors from 'cors'
 import express from 'express'
-import path from 'path'
 
 import { routes } from './routes'
 
@@ -15,10 +14,5 @@ app.use(cors())
 app.use(express.json())
 
 app.use(routes)
-app.get('/file', (request, response, next) => {
-  return response.sendFile(
-    path.join(__dirname, '..', 'tmp/10-2021-Bruninho.csv')
-  )
-})
 
 app.listen(3003, () => console.log('Run!'))
