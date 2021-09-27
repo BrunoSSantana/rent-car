97b7abb07b07a049d49d10adb162e5ed9fa5eb83
douglasans <douglasans@hotmail.com>
2021-09-27 06:24:05 -0300

    Register User e client funcionando

--- frontend/src/pages/RegisterClient/index.jsx
+++ frontend/src/pages/RegisterClient/index.jsx
@@ -1,4 +1,6 @@
-import React from "react";
+import React, { useState } from "react";
+import Axios from "axios";
+import { useHistory } from "react-router";
 
 import {
   Container,
@@ -13,19 +15,65 @@ import {
 } from "./styles";
 
 export default function RegisterClient() {
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [phone, setPhone] = useState("");
+
+  const history = useHistory();
+
+  async function registerClient() {
+    try {
+      await Axios.post("http://localhost:3003/clients/new", {
+        name,
+        email,
+        password,
+        tel: phone,
+      }).then((response) => {
+        console.log(response);
+        history.push("/");
+      });
+    } catch (error) {
+      console.log(error);
+    }
+  }
+
   return (
     <Wrapper>
       <Content>
         <Title>Que tal ter um carro para viajar</Title>
         <Container>
           <ContainerName>
-            <InputName placeholder="Nome" type="text" />
-            <InputLastName placeholder="Sobrenome" type="text" />
+            <InputName
+              placeholder="Nome"
+              type="text"
+              onChange={(e) => {
+                setName(e.target.value);
+              }}
+            />
           </ContainerName>
-          <InputLogin placeholder="Telefone" type="tel" />
-          <InputLogin placeholder="Email" type="email" />
-          <InputLogin placeholder="Senha" type="password" />
-          <Button>Entrar</Button>
+          <InputLogin
+            placeholder="Telefone"
+            type="tel"
+            onChange={(e) => {
+              setPhone(e.target.value);
+            }}
+          />
+          <InputLogin
+            placeholder="Email"
+            type="email"
+            onChange={(e) => {
+              setEmail(e.target.value);
+            }}
+          />
+          <InputLogin
+            placeholder="Senha"
+            type="password"
+            onChange={(e) => {
+              setPassword(e.target.value);
+            }}
+          />
+          <Button onClick={registerClient}>Cadastrar</Button>
         </Container>
       </Content>
     </Wrapper>
--- frontend/src/pages/RegisterUser/index.jsx
+++ frontend/src/pages/RegisterUser/index.jsx
@@ -1,4 +1,6 @@
-import React from "react";
+import React, { useState } from "react";
+import { useHistory } from "react-router";
+import Axios from "axios";
 
 import {
   Container,
@@ -13,18 +15,46 @@ import {
 } from "./styles";
 
 export default function RegisterUser() {
+
+  
+  const [name, setName] = useState('')
+  const [email, setEmail] = useState('')
+  const [password, setPassword] = useState('')
+
+  const history = useHistory()
+
+  async function registerUser() {
+    try {
+      await Axios.post("http://localhost:3003/users/new", {
+        name,
+        email,
+        password
+      }).then((response) => {
+        console.log(response);
+        history.push("/");
+      });
+    } catch (error) {
+      console.log(error);
+    }
+  }
+
   return (
     <Wrapper>
       <Content>
         <Title>Alugue seu carro com a RentCar</Title>
         <Container>
           <ContainerName>
-            <InputName placeholder="Nome" type="text" />
-            <InputLastName placeholder="Sobrenome" type="text" />
+            <InputName placeholder="Nome" type="text" onChange={(e)=>{
+              setName(e.target.value)
+            }}/>
           </ContainerName>
-          <InputLogin placeholder="Email" type="email" />
-          <InputLogin placeholder="Senha" type="password" />
-          <Button>Entrar</Button>
+          <InputLogin placeholder="Email" type="email" onChange={(e)=>{
+              setEmail(e.target.value)
+            }}/>
+          <InputLogin placeholder="Senha" type="password" onChange={(e)=>{
+              setPassword(e.target.value)
+            }}/>
+          <Button onClick={registerUser}>Entrar</Button>
         </Container>
       </Content>
     </Wrapper>
