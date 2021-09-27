11f099254b14142b2733edbb07df06ccffc97b22
douglasans <douglasans@hotmail.com>
2021-09-27 06:41:31 -0300

    Auth-login User e client funcionando

--- frontend/src/pages/LoginClient/index.jsx
+++ frontend/src/pages/LoginClient/index.jsx
@@ -1,15 +1,44 @@
 import React from "react";
 import { Container, Content, Input, Title, Wrapper, Button } from "./styles";
+import Axios from 'axios'
+import { useHistory } from "react-router";
+import { useState } from "react/cjs/react.development";
+
 
 export default function LoginClient() {
+
+  const [email, setEmail] = useState('')
+  const [password, setPassword] = useState('')
+
+  const history = useHistory()
+
+  async function loginClient() {
+    try {
+      await Axios.post("http://localhost:3003/clients/auth", {
+        email,
+        password
+      }).then((response) => {
+        console.log(response);
+        localStorage.setItem('token', response.data)
+        history.push("/client");
+      });
+    } catch (error) {
+      console.log(error);
+    }
+  }
+
   return (
     <Wrapper>
       <Content>
         <Title>Que tal ter um carro para viajar</Title>
         <Container>
-          <Input placeholder="Email" type="email" />
-          <Input placeholder="Senha" type="password" />
-          <Button>Entrar</Button>
+          <Input placeholder="Email" type="email" onChange={(e)=>{
+              setEmail(e.target.value)
+            }}/>
+          <Input placeholder="Senha" type="password" onChange={(e)=>{
+              setPassword(e.target.value)
+            }}/>
+          <Button onClick={loginClient}>Entrar</Button>
         </Container>
       </Content>
     </Wrapper>
--- frontend/src/pages/LoginUser/index.jsx
+++ frontend/src/pages/LoginUser/index.jsx
@@ -1,15 +1,42 @@
-import React from "react";
+import React, { useState } from "react";
 import { Container, Content, Input, Title, Wrapper, Button } from "./styles";
+import Axios from 'axios'
+import { useHistory } from "react-router";
 
 export default function LoginUser() {
+
+  const [email, setEmail] = useState('')
+  const [password, setPassword] = useState('')
+
+  const history = useHistory()
+
+  async function loginUser() {
+    try {
+      await Axios.post("http://localhost:3003/users/auth", {
+        email,
+        password
+      }).then((response) => {
+        console.log(response);
+        localStorage.setItem('token', response.data)
+        history.push("/user");
+      });
+    } catch (error) {
+      console.log(error);
+    }
+  }
+
   return (
     <Wrapper>
       <Content>
         <Title>Alugue seu carro com a RentCar</Title>
         <Container>
-          <Input placeholder="Email" type="email" />
-          <Input placeholder="Senha" type="password" />
-          <Button>Entrar</Button>
+          <Input placeholder="Email" type="email" onChange={(e)=>{
+              setEmail(e.target.value)
+            }}/>
+          <Input placeholder="Senha" type="password" onChange={(e)=>{
+              setPassword(e.target.value)
+            }}/>
+          <Button onClick={loginUser}>Entrar</Button>
         </Container>
       </Content>
     </Wrapper>
