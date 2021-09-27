a504cf53c931cce20e66a86461baaf834e0cd103
Jonata Ramalho <jonataramalho.s@gmail.com>
2021-09-26 23:38:52 -0300

    fix: adiciona container login

--- frontend/src/pages/Home/index.jsx
+++ frontend/src/pages/Home/index.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 
 import {
   Wrapper,
@@ -12,22 +12,48 @@ import {
 
 import logo from "../../assets/RentCar.svg";
 import user from "../../assets/user.svg";
-import { Informative } from "../../components";
+import { ContainerLogin, Informative } from "../../components";
 
 export default function Home() {
+  const [page, setPage] = useState(0);
+
+  function showInformative() {
+    return <Informative />;
+  }
+
+  function showContainerLogin() {
+    return <ContainerLogin />;
+  }
+
+  function increase(number) {
+    setPage(number);
+  }
+
+  function reset() {
+    setPage(0);
+  }
+
+  function renderPage() {
+    if (page === 0) {
+      return showInformative();
+    } else if (page === 1) {
+      return showContainerLogin();
+    }
+  }
+
   return (
     <Wrapper>
       <Header>
-        <Logo src={logo} alt="Logo RentCar" />
+        <Logo src={logo} alt="Logo RentCar" onClick={() => reset()} />
         <Menu>
-          <ButtonLogin>
+          <ButtonLogin onClick={() => increase(1)}>
             <Icon src={user} />
             Fazer Login
           </ButtonLogin>
           <ButtonRegister>Cadastre-se</ButtonRegister>
         </Menu>
       </Header>
-      <Informative />
+      {renderPage()}
     </Wrapper>
   );
 }
